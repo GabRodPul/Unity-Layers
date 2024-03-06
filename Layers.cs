9ecfc573d5b628f9b833e0cc58902c8493c66e5a
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2024-03-06 19:29:53 +0000

    Create Layers.cs

---
+++ Layers.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Static class containing const ints for Unity layers.
+/// Simply copy & paste.
+/// Meant to make switching on layers easier and all of that.
+/// </summary>
+public static class Layers
+{
+    public const int Default        = 0;
+    public const int TransparentFX  = 1;
+    public const int IgnoreRaycast  = 2;
+    // public const int Layer3         = 3;
+    public const int Water          = 4;
+    public const int UI             = 5;
+}
